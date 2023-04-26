ab0b5d9ab06926080e0fad44a96b4cf1d9b288d8
koplo199 <85577251+koplo199@users.noreply.github.com>
2023-04-26 09:52:41 +0000

    Replace remaining third party actions with native Github CLI commands + Implement idea from #233 + Automate Kron4ek wine builds pulling + Various improvements (#234)

--- input_files/update_yml_entries.sh
+++ input_files/update_yml_entries.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 nameprefix="${1-$NAMEPREFIX}"
-component_name="${2-$NAME}"
-category="${3-$CATEGORY}"
-subcategory="${4-$SUBCATEGORY}"
-channel="${5-$CHANNEL}"
-filename="${6-$YAML_FILENAME}"
-created_at="${7-$CREATED_AT}"
+namesuffix="${2-$NAMESUFFIX}"
+component_name="${3-$NAME}"
+category="${4-$CATEGORY}"
+subcategory="${5-$SUBCATEGORY}"
+channel="${6-$CHANNEL}"
+filename="${7-$YAML_FILENAME}"
+created_at="${8-$CREATED_AT}"
 
 filename=$(dirname "$0")/$filename
 created_at=$(date -d "$created_at" +%s)
 
 GITHUB_ENV=${GITHUB_ENV-/dev/null}
 
-command_wo_sub="{\"$component_name\": {\"Category\":\"$category\", \"Channel\": \"$channel\", \"Date\": \"$created_at\"}}"
-command_w_sub="{\"$component_name\": {\"Category\":\"$category\", \"Sub-category\":\"$subcategory\", \"Channel\": \"$channel\", \"Date\": \"$created_at\"}}"
+command_wo_sub='{\(\"$component_name\"\): {\"Category\":\"$category\", \"Channel\": \"$channel\", \"Date\": \"$created_at\"}}'
+command_w_sub='{\(\"$component_name\"\): {\"Category\":\"$category\", \"Sub-category\": \"$subcategory\", \"Channel\": \"$channel\", \"Date\": \"$created_at\"}}'
 
 empty_file=$(yq "." $filename)
 if ! [ -f "$filename" ] || [ -z "$empty_file" ]; then
     touch $filename
     if [ -z "$subcategory" ]; then
-        yq -n -i -y "$command_wo_sub" $filename
+        yq -n -i -y "$(eval echo $command_wo_sub)" $filename
     else
-        yq -n -i -y "$command_w_sub" $filename
+        yq -n -i -y "$(eval echo $command_w_sub)" $filename
     fi
     echo "Updated."
     echo "UPDATED=true" >> $GITHUB_ENV
@@ -31,9 +32,9 @@ fi
 
 create_entry () {
     if [ -z "$subcategory" ]; then
-        yq -i -y "$command_wo_sub + ." $filename
+        yq -i -y "$(eval echo $command_wo_sub) + ." $filename
     else
-        yq -i -y "$command_w_sub + ." $filename
+        yq -i -y "$(eval echo $command_w_sub) + ." $filename
     fi
 }
 
@@ -41,7 +42,7 @@ update_entry () {
     yq -i -y "with_entries(if .key == \"$latest\" then .key = \"$component_name\" else . end) | .\"$component_name\".Channel = \"$channel\" | .\"$component_name\".Date = \"$created_at\"" $filename
 }
 
-latest=$(yq -r 'path(.[])[0]' $filename | grep -m1 "$nameprefix")
+latest=$(yq -r 'path(.[])[0]' $filename | grep -E -m1 "$nameprefix.*[[:digit:]]+$namesuffix")
 
 if [ -z "$latest" ]; then
     create_entry
@@ -82,6 +83,15 @@ if [ "$newer" -eq 0 ]; then
     exit 1
 fi
 
+if [ "$channel" = "unstable" ] && [ "$latest_channel" = "unstable" ]; then
+    if [[ $latest =~ .*-([[:digit:]])-[[:alnum:]]{7} ]]; then
+        revision="${BASH_REMATCH[1]}"
+        commit_sha=${component_name: -7}
+        ((revision=$revision+1))
+        component_name=${component_name/-1-$commit_sha/-$revision-$commit_sha}
+        echo "NAME=$component_name" >> $GITHUB_ENV
+    fi
+fi
 
 if [ "$latest_channel" = "stable" ]; then
     create_entry
